1eb6602b68643fa3e3f891d7302db70bd083a82b
damon <dsy.chung@gmail.comn>
2022-02-28 19:28:11 +0800

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.swalDefaultSuccess').click(function() {
+        Toast.fire({
+          icon: 'success',
+          title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.'
+        })
+    });
+})
